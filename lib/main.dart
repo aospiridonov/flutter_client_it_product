2a3e2f53392eafc81ae10104aeec714f84a64042
aospiridonov <aospiridonov@gmail.com>
2022-11-18 11:03:06 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter_client_it_product/app/presentation/main_app_runner.dart'
 import 'package:flutter_client_it_product/app/presentation/main_app_builder.dart';
 
 void main() {
-  final env = String.fromEnvironment('env', defaultValue: 'test');
-  final runner = MainAppRunner(env);
+  const env = String.fromEnvironment('env', defaultValue: 'test');
+  const runner = MainAppRunner(env);
   final builder = MainAppBuilder();
   runner.run(builder);
 }
